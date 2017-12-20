451852522f788cb11a2f2943d713338180162f77
JOB <getabujob@gmail.com>
2017-12-20 16:10:04 +0300

    Receipt : details class

---
+++ eSchool/ReceiptPrints/StudentDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.ReceiptPrints
+{
+    class StudentDetails
+    {
+        public string StudentAdminNo { get; set; }
+
+        public string StudentName { get; set; }
+        public StudentDetails(Student_Basic stud)
+        {
+            StudentAdminNo = $"REG NO: {stud.Admin_No}";
+            StudentName = $"{stud.First_Name} {stud.Last_Name}";
+        }
+    }
+}
