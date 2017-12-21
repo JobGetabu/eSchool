3924efb7c10b048ba10c01c2118bbe47c503c1ff
JOB <getabujob@gmail.com>
2017-12-21 20:48:43 +0300

    Form Payment Receipt

---
+++ eSchool/ReceiptPrints/FrmPaymentReceipt.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace eSchool.ReceiptPrints
+{
+    partial class FrmPaymentReceipt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPaymentReceipt));
+            this.panelRpt = new System.Windows.Forms.Panel();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.PaymentOverheadsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panelRpt.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PaymentOverheadsBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelRpt
+            // 
+            this.panelRpt.Controls.Add(this.reportViewer);
+            this.panelRpt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRpt.Location = new System.Drawing.Point(0, 0);
+            this.panelRpt.Name = "panelRpt";
+            this.panelRpt.Size = new System.Drawing.Size(812, 542);
+            this.panelRpt.TabIndex = 1;
+            // 
+            // reportViewer
+            // 
+            this.reportViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet2";
+            reportDataSource1.Value = this.PaymentOverheadsBindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "eSchool.ReceiptPrints.PaymentReceipt.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.Padding = new System.Windows.Forms.Padding(10);
+            this.reportViewer.PageCountMode = Microsoft.Reporting.WinForms.PageCountMode.Actual;
+            this.reportViewer.ShowBackButton = false;
+            this.reportViewer.ShowContextMenu = false;
+            this.reportViewer.ShowCredentialPrompts = false;
+            this.reportViewer.ShowDocumentMapButton = false;
+            this.reportViewer.ShowFindControls = false;
+            this.reportViewer.Size = new System.Drawing.Size(812, 542);
+            this.reportViewer.TabIndex = 0;
+            // 
+            // PaymentOverheadsBindingSource
+            // 
+            this.PaymentOverheadsBindingSource.DataSource = typeof(eSchool.ReceiptPrints.PaymentOverheads);
+            // 
+            // FrmPaymentReceipt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 542);
+            this.Controls.Add(this.panelRpt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmPaymentReceipt";
+            this.Text = "Payment Receipt";
+            this.Load += new System.EventHandler(this.FrmPaymentReceipt_Load);
+            this.panelRpt.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PaymentOverheadsBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelRpt;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.BindingSource PaymentOverheadsBindingSource;
+    }
+}
---
+++ eSchool/ReceiptPrints/FrmPaymentReceipt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eSchool.ReceiptPrints
+{
+    public partial class FrmPaymentReceipt : Form
+    {
+        public FrmPaymentReceipt()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPaymentReceipt_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
