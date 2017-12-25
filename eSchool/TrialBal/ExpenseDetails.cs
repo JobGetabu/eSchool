e08fe20741cd12f35ab3888f6ad8e8cb310f0189
JOB <getabujob@gmail.com>
2017-12-25 22:48:02 +0300

    Trial Bal Report Layout

---
+++ eSchool/TrialBal/ExpenseDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.TrialBal
+{
+    public class ExpenseDetails
+    {
+        public string ExpenseName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
---
+++ eSchool/TrialBal/IncomeDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSchool.TrialBal
+{
+    public class IncomeDetails
+    {
+        public string IncomeName { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
